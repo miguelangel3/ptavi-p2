978a7ea335ed5e713ad05a34705a677f5b3d664d
miguelangel3 <miguelangel.abb@gmail.com>
2017-09-27 20:52:27 +0200

    Creo la clase CalculadoraHija y los métodos multiplica y divide

---
+++ calcoohija.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class CalculadoraHija():
+	def __init__(self, arg):
+		pass
+
+	def multiplica(self,operando1,operando2):
+		return operando1 * operando2
+
+	def divide(self,operando1,operando2):
+		return operando1 / operando2
+		
