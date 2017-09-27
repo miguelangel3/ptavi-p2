c178431282d3135165efac39d213bec2af122b4d
miguelangel3 <miguelangel.abb@gmail.com>
2017-09-27 20:32:33 +0200

    Arreglo tabulado

--- calcoo.py
+++ calcoo.py
@@ -3,31 +3,28 @@
 
 import sys
 
-class Clase(Calculadora):
-  "Esto es un ejemplo de clase que hereda de ClaseMadre"
+class Calculadora():
 
-	def __init__(self, valor):
-    "Esto es el método iniciliazador"
-    self.atributo = valor
-    def suma (self,operando1,operando2):
+
+    def suma(self,operando1,operando2):
     	return operando1 + operando2
 
-    def resta (self,operando1,operando2):
+    def resta(self,operando1,operando2):
     	return operando1 - operando2
 
 if __name__ == "__main__":
-  
+
 	try:
-    	operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
 
-    if sys.argv[2] == "suma":
-        result = suma(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        result = resta(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
+	if sys.argv[2] == "suma":
+		result = Calculadora.suma(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		result = Calculadora.resta(operando1, operando2)
+	else:
+		sys.exit('Operación sólo puede ser sumar o restar.')
 
-    print(result)
+	print(result)
