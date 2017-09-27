aaa84ca34aa90530c43f45d342eecb59b89574c6
miguelangel3 <miguelangel.abb@gmail.com>
2017-09-27 21:04:41 +0200

    Añado el main y la excepción 0

--- calcoohija.py
+++ calcoohija.py
@@ -11,5 +11,28 @@ class CalculadoraHija():
 		return operando1 * operando2
 
 	def divide(self,operando1,operando2):
-		return operando1 / operando2
-		
+		try 
+			return operando1 / operando2
+		except ZeroDivisionError
+			sys.exit("No se puede dividir entre 0") 
+
+if __name__ == '__main__':
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+
+	except ValueError:
+		sys.exit("Los operandos tienen que ser enteros")
+
+	if operador == "suma":
+		resultado = 
+	elif operador == "resta":
+		resultado = 
+	elif operador == "multiplica":
+		resultado = CalculadoraHija().multiplica(operando1,operando2)
+	elif operador == "divide":
+		resultado = CalculadoraHija().divide(operando1,operando2)
+	else:
+		sys.exit("El operador solo puede ser sumar,restar,multiplicar o dividir")
+
+	print ("El resultado de " + sys.argv +"es: ", (resultado))
