f72ccbc5e85f0c0ab0950778c9d05ae248d1e62d
miguelangel3 <miguelangel.abb@gmail.com>
2017-09-30 19:57:15 +0200

    Elimino las funciones de prueba y limpio el código

--- calcplus.py
+++ calcplus.py
@@ -14,33 +14,12 @@ cal = {
 }
 
 
-def operando (line,operacionfind):
-	#for string in line:
-	pass
-
-def resta (val1,val2):
-	pass
-
-def divide(val1,val2):
-	pass
-
-def multiplica (val1,val2):
-	pass
-
-#def suma (val1,val2):
-
-#	return val1 + val2
-	
-def tostring (line):
-	pass
-
 def calcula_linea (file_descriptor):
 
 	for line in file_descriptor.readlines():
 
 		calcular = line.split(",")
 		resultado = int(calcular[1])
-		calcular = line.split(",")
 		operacion = cal[calcular[0]]
 
 		for calcula in calcular [2:]:
@@ -52,9 +31,4 @@ def calcula_linea (file_descriptor):
 if __name__ == '__main__':
 
 	file_descriptor = open("fichero")
-	dicionario = {}
-	operando = {}
-
 	calcula_linea(file_descriptor)
-
-	#print (operacion)
