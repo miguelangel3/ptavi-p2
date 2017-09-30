e8fa748f53c998091d93b12b6b95de0c549c020a
miguelangel3 <miguelangel.abb@gmail.com>
2017-09-30 13:32:47 +0200

    CAmbio la función readline por calculalinea ahora hago un split para separar las operaciones y los operandos de las comas

--- calcplus.py
+++ calcplus.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import calcoo
+import calcoohija
+
+cal = [calcoo,calcoohija]
+
+def operando (line,operacionfind):
+	#for string in line:
+	pass
+def operacion (line):
+
+	return line[:line.find(",")]
+	#return operacion = line[:line.find(",")] 
+
+def tostring (line):
+	pass
+
+def readlines(file_descriptor):
+
+	for line in file_descriptor.readlines():
+		#operacion = line[:line.find(",")]
+		calcular = line.split(",")
+
+		operación = 
+
+
+
+		print (calcular)
+
 if __name__ == '__main__':
 
 	file_descriptor = open("fichero")
 	dicionario = {}
+	operando = {}
 
-	for line in file_descriptor.readlines():
-		operacion = line[:line.find(",")]
+	readlines(file_descriptor)
 
-		print (operacion)
+	#print (operacion)
