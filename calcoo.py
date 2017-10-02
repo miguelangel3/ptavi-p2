66723467febf18bbea7a6cda9f5e1d3be8c4cab2
Miguel Ngel Alba Blanco <maalbab@gamma25.aulas.gsyc.urjc.es>
2017-10-02 15:46:27 +0200

    Arreglo fallos de sintaxis que me ha dado pep8

--- calcoo.py
+++ calcoo.py
@@ -3,28 +3,28 @@
 
 import sys
 
+
 class Calculadora():
 
+    def suma(self, operando1, operando2):
+        return operando1 + operando2
 
-    def suma(self,operando1,operando2):
-    	return operando1 + operando2
-
-    def resta(self,operando1,operando2):
-    	return operando1 - operando2
+    def resta(self, operando1, operando2):
+        return operando1 - operando2
 
 if __name__ == "__main__":
 
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-	if sys.argv[2] == "suma":
-		result = Calculadora.suma(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		result = Calculadora.resta(operando1, operando2)
-	else:
-		sys.exit('Operación sólo puede ser sumar o restar.')
+    if sys.argv[2] == "suma":
+        result = Calculadora.suma(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = Calculadora.resta(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
 
-	print(result)
+    print(result)
